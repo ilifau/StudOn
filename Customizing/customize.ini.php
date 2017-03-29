f60e3c37e1fc55f49b17cf6260edb4ac27290f42
Fred Neumann <fred.neumann@fau.de>
2017-03-29 17:04:24 +0200

    testPreventNavModal

--- Customizing/customize.ini.php
+++ Customizing/customize.ini.php
@@ -129,6 +129,7 @@ tst_notify_remote = "0"                 ;send mails to users at remote installat
 tst_export_mycampus = "0"               ;enable the export of test results for my campus
 tst_prevent_image_drag = "0"			;prevent the dragging of images an links to texts fields in tests
 tst_prevent_media_player = "0"			;prevent the embedding of the media element player
+tst_prevent_nav_modal = "0"             ;prevent the navigation modal
 
 univis_server = ""                      ;only the server name, e.g. univis.uni-erlangen.de
 univis_port = ""                        ;port sor testing socket connection in univis2mysql
