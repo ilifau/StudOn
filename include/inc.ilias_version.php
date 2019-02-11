f768e35bb3b2fc60a328dde083bacf391747ef06
Fred Neumann <fred.neumann@fau.de>
2019-02-11 14:34:44 +0100

    version suffix

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -14,6 +14,6 @@ define("ILIAS_VERSION_NUMERIC", "5.3.10");			// must be always x.y.z: x, y and z
 
 // fau: versionSuffix - define a version with suffix for including css and js files
 // please increase a suffix number if a css or js file is locally changed!
-define("ILIAS_VERSION_SUFFIX", ILIAS_VERSION_NUMERIC . ".20");
+define("ILIAS_VERSION_SUFFIX", ILIAS_VERSION_NUMERIC . ".21");
 // fau.
 ?>
