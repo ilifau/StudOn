1b89ed9fef06d4c4816908136b1b49e72056c74b
Fred Neumann <fred.neumann@fau.de>
2019-03-04 10:59:00 +0100

    fau: faurSub - fix query for auto-fill

--- Modules/Course/classes/class.ilObjCourse.php
+++ Modules/Course/classes/class.ilObjCourse.php
@@ -2439,6 +2439,8 @@ class ilObjCourse extends ilContainer implements ilMembershipRegistrationCodes
 	{
 		global $ilDB;
 
+        // find all groups with a finished fair period in the last month
+        // that are not filled or last filled before the fair period
 		$query = "
 			SELECT s.obj_id
 			FROM crs_settings s
@@ -2451,9 +2453,9 @@ class ilObjCourse extends ilContainer implements ilMembershipRegistrationCodes
 			AND s.sub_mem_limit > 0
 			AND s.sub_max_members > 0 
 			AND s.sub_auto_fill > 0
-			AND s.sub_fair > 0
+			AND s.sub_fair > (UNIX_TIMESTAMP() - 3600 * 24 * 30)
 			AND s.sub_fair < UNIX_TIMESTAMP()
-			AND s.sub_last_fill IS NULL
+			AND (s.sub_last_fill IS NULL OR s.sub_last_fill < s.sub_fair)
 		";
 
 		$obj_ids = array();
--- Modules/Group/classes/class.ilObjGroup.php
+++ Modules/Group/classes/class.ilObjGroup.php
@@ -2327,6 +2327,8 @@ class ilObjGroup extends ilContainer implements ilMembershipRegistrationCodes
 	{
 		global $ilDB;
 
+		// find all groups with a finished fair period in the last month
+		// that are not filled or last filled before the fair period
 		$query = "
 			SELECT s.obj_id 
 			FROM grp_settings s
@@ -2336,9 +2338,9 @@ class ilObjGroup extends ilContainer implements ilMembershipRegistrationCodes
 			AND registration_mem_limit > 0 
 			AND registration_max_members > 0
 			AND s.sub_auto_fill > 0
-			AND sub_fair > 0
+			AND s.sub_fair > (UNIX_TIMESTAMP() - 3600 * 24 * 30)
 			AND sub_fair < UNIX_TIMESTAMP()
-			AND sub_last_fill IS NULL
+			AND (s.sub_last_fill IS NULL OR s.sub_last_fill < s.sub_fair)
 		";
 
 		$obj_ids = array();
