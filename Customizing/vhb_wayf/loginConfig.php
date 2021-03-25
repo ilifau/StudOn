e4ee19416343432f84e37059d490ef3a4c3523c2
Fred Neumann <fred.neumann@fau.de>
2021-03-25 17:53:39 +0100

    added vhb WAYF

---
+++ Customizing/vhb_wayf/loginConfig.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * WAYF Konfigurationdatei
+ */
+
+$CONFIG = [
+
+    'wayf_config' =>[
+        'wayf_sp_handlerURL' => 'https://www.studon.fau.de/studon/VHBSSO/Shibboleth.sso',
+        'wayf_return_url' => 'https://www.studon.fau.de/studon/VHBSSO',
+        'wayf_sp_entityID' => 'https://www.studon.fau.de/studon/VHBSSO/Metadata',
+        'wayf_base_url' => 'https://idp.vhb.org/wayf',
+        'submitButtonText' => 'Einloggen',
+        'textAbove' => 'Heimathochschule'
+    ]
+];
