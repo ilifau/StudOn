0a4b80a9124a293135e423446e66db8a0201c999
Cornel Musielak <cornel.musielak@gmail.com>
2022-04-06 06:45:16 +0200

    dclFixMissingFieldType: update description

--- Modules/DataCollection/classes/Fields/Selection/class.ilDclSelectionFieldRepresentation.php
+++ Modules/DataCollection/classes/Fields/Selection/class.ilDclSelectionFieldRepresentation.php
@@ -88,10 +88,9 @@ abstract class ilDclSelectionFieldRepresentation extends ilDclBaseFieldRepresent
                 }
                 $input->setOptions($array);
                 break;
-            //fau
-            //dcl_fix_missing_fieldtype: if no type is set-> use combobox
+            // fau: dclFixMissingFieldType: if no type is set -> use combobox
             default:
-            //fau
+            // fau.
                 $input = new ilSelectInputGUI($this->getField()->getTitle(), 'field_' . $this->getField()->getId());
                 $array = array();
                 foreach ($options as $opt) {
