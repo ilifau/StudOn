2f994a8f4956ce2dd068bab79c9a4d436175694c
Fred Neumann <fred.neumann@fau.de>
2022-04-13 13:06:21 +0200

    keepAllLocalChanges: don't set a change date for the local file (treat it like an original ilias file)

--- Services/Language/classes/Setup/class.ilSetupLanguage.php
+++ Services/Language/classes/Setup/class.ilSetupLanguage.php
@@ -509,8 +509,8 @@ class ilSetupLanguage extends ilLanguage
                     // set the change date to import time for a local file
                     // get the modification date of the local file
                     // get the newer local changes for a local file
-                    $change_date = date("Y-m-d H:i:s", time());
-                    // fau: keepAllLocalChanges - don't use the file date of the local language file
+                    // fau: keepAllLocalChanges - import local file without change date, find local changes without restriction
+                    $change_date = null;
                     $local_changes = $this->getLocalChanges($lang_key);
                     // fau.
                 }
