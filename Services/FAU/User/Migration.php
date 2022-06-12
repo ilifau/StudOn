2d848e8dff9fd2d74a2d2c2bc4f0a472e66b6776
Fred Neumann <fred.neumann@fau.de>
2022-06-12 18:47:51 +0200

    fauService: creation of persons table

--- Services/FAU/User/Migration.php
+++ Services/FAU/User/Migration.php
@@ -14,6 +14,7 @@ class Migration
     public function createTables(bool $drop = false) {
         $this->createUserAchievementsTable($drop);
         $this->createUserEducationsTable($drop);
+        $this->createUserPersonsTable($drop);
     }
 
     protected function createUserAchievementsTable(bool $drop = false)
@@ -21,7 +22,7 @@ class Migration
         $this->db->createTable('fau_user_achievements', [
             'person_id'       => ['type' => 'integer',    'length' => 4,      'notnull' => true],
             'requirement_id'  => ['type' => 'integer',    'length' => 4,      'notnull' => true],
-       ],
+            ],
             $drop
         );
         $this->db->addPrimaryKey('fau_user_achievements', ['person_id', 'requirement_id']);
@@ -42,4 +43,22 @@ class Migration
         $this->db->addPrimaryKey('fau_user_educations', ['user_id', 'type', 'key']);
     }
 
+    protected function createUserPersonsTable(bool $drop = false)
+    {
+        $this->db->createTable('fau_user_persons', [
+            'user_id'               => ['type' => 'integer',    'length' => 4,      'notnull' => true],
+            'person_id'             => ['type' => 'integer',    'length' => 4,      'notnull' => true],
+            'employee'              => ['type' => 'text',       'length' => 250,    'notnull' => false, 'default' => null],
+            'student'               => ['type' => 'text',       'length' => 250,    'notnull' => false, 'default' => null],
+            'guest'                 => ['type' => 'text',       'length' => 250,    'notnull' => false, 'default' => null],
+            'doc_programmes_text'   => ['type' => 'text',       'length' => 250,    'notnull' => false, 'default' => null],
+            'doc_programmes_code'   => ['type' => 'integer',    'length' => 4,      'notnull' => false, 'default' => null],
+            'studydata'             => ['type' => 'clob',                           'notnull' => false, 'default' => null],
+            'orgdata'               => ['type' => 'clob',                           'notnull' => false, 'default' => null],
+            ],
+            $drop
+        );
+        $this->db->addPrimaryKey('fau_user_persons', ['user_id']);
+        $this->db->addIndex('fau_user_persons', ['person_id'], 'i1');
+    }
 }
