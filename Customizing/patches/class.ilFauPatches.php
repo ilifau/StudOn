8a75f911a72ee5cc5e29e8c26d938bae2081fa1d
Fred Neumann <fred.neumann@fau.de>
2022-06-20 13:51:06 +0200

    fauService: dic in patches

--- Customizing/patches/class.ilFauPatches.php
+++ Customizing/patches/class.ilFauPatches.php
@@ -1,38 +1,59 @@
 <?php
+
+use ILIAS\DI\Container;
+
 /**
  * fau: fauService - patch to create the tables
  */
 class ilFauPatches
 {
-	public function createFauTables()
-	{
+
+    protected Container $dic;
+
+
+    public function __construct()
+    {
         global $DIC;
-        $DIC->fau()->cond()->migration()->createTables(true);
-        $DIC->fau()->org()->migration()->createTables(true);
-        $DIC->fau()->study()->migration()->createTables(true);
-        $DIC->fau()->user()->migration()->createTables(true);
+        $this->dic = $DIC;
+    }
+
+    public function createFauTables()
+	{
+        $this->dic->fau()->cond()->migration()->createTables(true);
+        $this->dic->fau()->org()->migration()->createTables(true);
+        $this->dic->fau()->study()->migration()->createTables(true);
+        $this->dic->fau()->user()->migration()->createTables(true);
 	}
 
 
+    public function migrateConditions()
+    {
+        $this->dic->fau()->cond()->migration()->fillCosConditionsFromStudydata($this->dic->fau()->staging()->database());
+        $this->dic->fau()->cond()->migration()->fillDocConditionsFromStudydata();
+    }
+
+    /**
+     * temporary
+     */
     public function createOrgTable()
     {
-        global $DIC;
-        $DIC->fau()->org()->migration()->createTables(true);
+        $this->dic->fau()->org()->migration()->createTables(true);
     }
 
 
+    /**
+     * temporary
+     */
     public function syncOrgTable()
     {
-        global $DIC;
-        $service = $DIC->fau()->sync()->org();
+        $service = $this->dic->fau()->sync()->org();
         $service->synchronize();
     }
 
 
     public function checkOrgUnitRelations()
     {
-        global $DIC;
-        $service = $DIC->fau()->org();
+        $service = $this->dic->fau()->org();
         $service->checkOrgUnitRelations();
     }
 
