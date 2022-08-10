107de4cb10523af34d37dc583166b7c4ff5ca30a
Fred Neumann <fred.neumann@fau.de>
2022-08-10 22:41:05 +0200

    fixed default setting for campus and campo test export

--- Customizing/customize.ini.php
+++ Customizing/customize.ini.php
@@ -91,8 +91,8 @@ shib_devmode_login = ""                 ;force a login at shibboleth authenticat
 studydata_check_ref_ids = "";           ;list of ref_ids for which studydata are checked (comma-separated without spaces)
 
 tst_notify_remote = "0"                 ;send mails to users at remote installation (for exam)
-tst_export_mycampus = "1"               ;enable the export of test results for my campus
-tst_export_campo = "1"                  ;enable the export of test results for campo
+tst_export_mycampus = "0"               ;enable the export of test results for my campus
+tst_export_campo = "0"                  ;enable the export of test results for campo
 tst_prevent_image_drag = "0"			;prevent the dragging of images an links to texts fields in tests
 tst_prevent_image_validate = "0"        ;fau: preventQtiImageValidate - prevent validation of images at test/pool import
 
