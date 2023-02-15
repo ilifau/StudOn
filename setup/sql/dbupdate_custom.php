76c355c063d642a25475b4ec3888223d1442cc1a
Fred Neumann <fred.neumann@fau.de>
2023-02-15 16:44:40 +0100

    fauService - create update step for event types table

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -1692,7 +1692,14 @@ if (!$ilDB->tableColumnExists('crs_waiting_list', 'module_id')) {
 <#116>
 <?php
 /**
- * fau: fauService - create the event types table
+ * fau: fauService - create the terms table
  */
 \FAU\Setup\Setup::instance($ilDB)->study()->custom_step_116();
+?>
+<#117>
+<?php
+/**
+ * fau: fauService - create the event types table
+ */
+\FAU\Setup\Setup::instance($ilDB)->study()->custom_step_117();
 ?>
