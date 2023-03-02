e8938a546b52eb9f4debccc401b9817b7d6fcdcf
Christina Fuchs <christina.fuchs@ili.fau.de>
2023-03-02 13:19:03 +0100

    fau: PassedFlagCG - improvements and language vars for course

--- Modules/Course/classes/class.ilCourseParticipantsTableGUI.php
+++ Modules/Course/classes/class.ilCourseParticipantsTableGUI.php
@@ -146,7 +146,7 @@ class ilCourseParticipantsTableGUI extends ilParticipantTableGUI
         $this->addMultiCommand('confirmDeleteParticipants', $this->lng->txt('remove'));
         $this->addMultiCommand('sendMailToSelectedUsers', $this->lng->txt('mmbr_btn_mail_selected_users'));
         // fau: PassedFlagCG
-        $this->addMultiCommand('bulkSetPassedFlag', $this->lng->txt('bulk_set_passed_flag'));
+        $this->addMultiCommand('bulkSetPassedFlag', $this->lng->txt('crs_bulk_set_passed_flag'));
         // fau.
         $this->lng->loadLanguageModule('user');
         $this->addMultiCommand('addToClipboard', $this->lng->txt('clipboard_add_btn'));
