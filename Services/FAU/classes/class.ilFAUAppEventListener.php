21eac051684e1210ee2b2481ab8533da740393f7
Fred Neumann <fred.neumann@fau.de>
2023-08-08 14:26:23 +0200

    cascadeMembers - automatically add or remove member in child or parent

--- Services/FAU/classes/class.ilFAUAppEventListener.php
+++ Services/FAU/classes/class.ilFAUAppEventListener.php
@@ -172,22 +172,30 @@ class ilFAUAppEventListener implements ilAppEventListener
 
     /**
      * Handle the adding of a participant to a course or group
+     * (called for courses and groups)
      */
     protected function handleAddParticipant(int $obj_id, int $user_id, int $role_id)
     {
         if ($role_id == IL_CRS_MEMBER || $role_id == IL_GRP_MEMBER) {
             $this->dic->fau()->user()->saveMembership($obj_id, $user_id);
         }
+        // fau: cascadeMembers - add as members to parent courses and groups
+        $this->addParticipantToParents($obj_id, $user_id);
+        // fau.
     }
 
     /**
      * Handle the deletion of a participant to a course or group
+     * (called for courses and groups)
      */
     protected function handleDeleteParticipant(int $obj_id, int $user_id, int $role_id)
     {
         if ($role_id == IL_CRS_MEMBER || $role_id == IL_GRP_MEMBER) {
             $this->dic->fau()->user()->deleteMembership($obj_id, $user_id);
         }
+        // fau: cascadeMembers - add as members to parent courses and groups
+        $this->removeParticipantFromChildren($obj_id, $user_id);
+        // fau.
     }
 
     /**
@@ -212,6 +220,77 @@ class ilFAUAppEventListener implements ilAppEventListener
             $title = ilObject::_lookupTitle($role_id);
             if (in_array(substr($title, 0, 14), ['il_crs_member_', 'il_grp_member_'])) {
                 $this->dic->fau()->user()->deleteMembership($obj_id, $user_id);
+            }
+        }
+    }
+
+    /**
+     * Ensure that an added participant is also added to the parent course or groups
+     * 
+     * @param int $obj_id
+     * @param int $user_id
+     */
+    protected function addParticipantToParents(int $obj_id, int $user_id): void
+    {
+        // prevent recursive calls in the loop
+        $this->setActive(false);
+        
+        $ref_id = $this->dic->fau()->ilias()->objects()->getUntrashedReference($obj_id);
+        if (isset($ref_id)) {
+            $path = $this->dic->repositoryTree()->getPathId($ref_id);
+            foreach ($path as $path_id) {
+                if ($path_id == $ref_id) {
+                    continue;
+                }
+                $type = ilObject::_lookupType($path_id, true);
+                if (in_array($type, ['crs', 'grp'])) {
+                    if (!ilParticipants::_isParticipant($path_id, $user_id)) {
+                        $path_obj_id = ilObject::_lookupObjId($path_id);
+                        switch ($type) {
+                            case 'crs':
+                                $participants = ilCourseParticipants::_getInstanceByObjId($path_obj_id);
+                                $participants->add($user_id, IL_CRS_MEMBER);
+                            case 'grp':
+                                $participants = ilGroupParticipants::_getInstanceByObjId($path_obj_id);
+                                $participants->add($user_id, IL_GRP_MEMBER);
+                        }
+                    }
+                }
+            }
+        }
+        
+        $this->setActive(true);
+    }
+
+    /**
+     * Ensure that a removed participant is also removed from child objects
+     * This will recourse if a participant is removed from a child object
+     * @param int $obj_id
+     * @param int $user_id
+     * @return void
+     */
+    protected function removeParticipantFromChildren(int $obj_id, int $user_id) : void
+    {
+        $ref_id = $this->dic->fau()->ilias()->objects()->getUntrashedReference($obj_id);
+        if (isset($ref_id)) {
+            $nodes = $this->dic->repositoryTree()->getChildsByTypeFilter($ref_id, ['grp', 'sess', 'lso']);
+            foreach($nodes as $node) {
+                if (ilParticipants::_isParticipant($node['child'], $user_id)) {
+                    switch ($node['type']) {
+                        case 'grp':
+                            $participants = ilGroupParticipants::_getInstanceByObjId($node['obj_id']);
+                            $participants->delete($user_id);
+                            break;
+                        case 'sess':
+                            $participants = ilSessionParticipants::_getInstanceByObjId($node['obj_id']);
+                            $participants->delete($user_id);
+                            break;
+                        case 'lso':
+                            $participant = ilLearningSequenceParticipants::_getInstanceByObjId($node['obj_id']);
+                            $participant->delete($user_id);
+                            break;
+                    }
+                }
             }
         }
     }
