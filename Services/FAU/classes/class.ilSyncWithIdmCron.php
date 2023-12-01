871fa15aa9727f06eb92f8173b41b4a39f6fd649
Christina Fuchs <chr.fuchs@fau.de>
2023-12-01 13:24:07 +0100

    chrfau/StudOn8#43 Declaration of ilSyncWithIdmCron::getDescription() must be compatible with ilCronJob::getDescription(): string

--- Services/FAU/classes/class.ilSyncWithIdmCron.php
+++ Services/FAU/classes/class.ilSyncWithIdmCron.php
@@ -18,7 +18,7 @@ class ilSyncWithIdmCron extends ilCronJob
         return $DIC->language()->txt("fau_idm_data_update");
     }
     
-    public function getDescription()
+    public function getDescription(): string
     {
         global $DIC;
         
