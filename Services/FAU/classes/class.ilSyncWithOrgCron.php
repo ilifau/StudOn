c940fbb6bfba618ecab6546bc1ceff8b58813b1f
Christina Fuchs <chr.fuchs@fau.de>
2023-12-01 14:29:16 +0100

    chrfau/StudOn8#55 Declaration of ilSyncWithOrgCron::getDefaultScheduleValue() must be compatible with ilCronJob::getDefaultScheduleValue(): ?int

--- Services/FAU/classes/class.ilSyncWithOrgCron.php
+++ Services/FAU/classes/class.ilSyncWithOrgCron.php
@@ -30,7 +30,7 @@ class ilSyncWithOrgCron extends ilCronJob
         return self::SCHEDULE_TYPE_IN_HOURS;
     }
     
-    public function getDefaultScheduleValue()
+    public function getDefaultScheduleValue(): ?int
     {
         return 24;
     }
