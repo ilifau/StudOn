f8036e3d9f447ab1227d68b040b557c5e21c0deb
Christina Fuchs <chr.fuchs@fau.de>
2023-12-01 11:47:05 +0100

    chrfau/StudOn8#31 Declaration of ilSyncToCampoCron::getDefaultScheduleValue() must be compatible with ilCronJob::getDefaultScheduleValue(): ?int

--- Services/FAU/classes/class.ilSyncToCampoCron.php
+++ Services/FAU/classes/class.ilSyncToCampoCron.php
@@ -30,7 +30,7 @@ class ilSyncToCampoCron extends ilCronJob
         return self::SCHEDULE_TYPE_IN_MINUTES;
     }
     
-    public function getDefaultScheduleValue()
+    public function getDefaultScheduleValue(): ?int
     {
         return 1;
     }
