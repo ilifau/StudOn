f31e905f385f18478ed494ce90daa521f743dd96
Christina Fuchs <chr.fuchs@fau.de>
2023-12-01 12:05:28 +0100

    chrfau/StudOn8#35 ilSyncWithCampoCron::getDescription() must be compatible with ilCronJob::getDescription(): string

--- Services/FAU/classes/class.ilSyncWithCampoCron.php
+++ Services/FAU/classes/class.ilSyncWithCampoCron.php
@@ -18,7 +18,7 @@ class ilSyncWithCampoCron extends ilCronJob
         return $DIC->language()->txt("fau_campo_data_update");
     }
     
-    public function getDescription()
+    public function getDescription(): string
     {
         global $DIC;
         
