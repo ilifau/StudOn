6133a3bd46ed3e19379975a408d1d489a2ff7980
Christina Fuchs <chr.fuchs@fau.de>
2023-12-06 16:01:38 +0100

    fix using of ctrl and lng vars in fau: studySearch

--- Services/Search/classes/class.ilMainMenuSearchGUI.php
+++ Services/Search/classes/class.ilMainMenuSearchGUI.php
@@ -139,8 +139,8 @@ class ilMainMenuSearchGUI
         }
 
         // fau: studySearch - link the search GUI
-        $this->tpl->setVariable('HREF_FAU_SEARCH_LINK', $ilCtrl->getLinkTargetByClass('fauStudySearchGUI'));
-        $this->tpl->setVariable('TXT_FAU_SEARCH_LINK', $lng->txt("fau_search"));
+        $this->tpl->setVariable('HREF_FAU_SEARCH_LINK', $this->ctrl->getLinkTargetByClass('fauStudySearchGUI'));
+        $this->tpl->setVariable('TXT_FAU_SEARCH_LINK', $this->lng->txt("fau_search"));
         // fau.
 
         // #10555 - we need the overlay for the autocomplete which is always active
