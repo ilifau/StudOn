1bd2068b3d01e1924b09c5ee8a79543179e9cd94
iszmais <iszmais@databay.de>
2024-05-27 14:07:07 +0200

    Excape version redeclaration to prevent notice

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -12,6 +12,6 @@
 // lts-patch: begin version
 define("ILIAS_VERSION", "7.30.1 2024-05-21");
 define("ILIAS_VERSION_NUMERIC", "7.30.1");
+//define("ILIAS_VERSION", "7.30 2024-05-14");
+//define("ILIAS_VERSION_NUMERIC", "7.30");			// since version ILIAS 6 this must be always x.y: x and y are numbers
 // lts-patch: end version
-define("ILIAS_VERSION", "7.30 2024-05-14");
-define("ILIAS_VERSION_NUMERIC", "7.30");			// since version ILIAS 6 this must be always x.y: x and y are numbers
