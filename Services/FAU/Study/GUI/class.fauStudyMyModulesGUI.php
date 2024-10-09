b0ec1faaaa8d2ca6d1393b717dec22e32e543684
Christina Fuchs <chr.fuchs@fau.de>
2024-10-09 14:13:26 +0200

    some debug code

--- Services/FAU/Study/GUI/class.fauStudyMyModulesGUI.php
+++ Services/FAU/Study/GUI/class.fauStudyMyModulesGUI.php
@@ -303,7 +303,7 @@ class fauStudyMyModulesGUI extends BaseGUI implements ilCtrlBaseClassInterface
 
             $html = '<p>' .$this->lng->txt('fau_rest_hard_restrictions') . ': '
                 . fauHardRestrictionsGUI::getInstance()->getResultModalLink($hardRestrictions, $selected_module_id) . '</p>';
-            $fp = fopen(ILIAS_HTTP_PATH. '/debug.txt', 'w');
+            $fp = fopen($_SERVER["DOCUMENT_ROOT"]. '/debug.txt', 'w');
             $test = array(1, 2);
             fwrite($fp, '<pre>'. var_dump($test).'</pre>');
             fclose($fp);  
