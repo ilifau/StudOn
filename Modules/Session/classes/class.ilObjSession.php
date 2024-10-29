eed5079a8dfef1bfe959781c7ee4da2ba4cbb333
chrfau <chr.fuchs@fau.de>
2024-10-29 14:29:24 +0100

    fix Call to undefined method ilObjSession::hasParallelGroups

--- Modules/Session/classes/class.ilObjSession.php
+++ Modules/Session/classes/class.ilObjSession.php
@@ -1086,4 +1086,23 @@ class ilObjSession extends ilObject
     {
         return false;
     }
+
+    // fau: paraSub - functions for checking parallel group relationships
+    /**
+     * Check if the object is a parallel group
+     */
+    public function isParallelGroup()
+    {
+        return  false;
+    }
+
+
+    /**
+     * Check if the object is a parallel group
+     */
+    public function hasParallelGroups()
+    {
+        return false;
+    }
+    // fau.    
 }
