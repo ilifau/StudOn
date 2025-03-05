b92339100049d8a0ec902a35c2ae5559d4bbaf43
Cornel Musielak <cornel.musielak@fau.de>
2025-03-05 07:46:27 +0100

    extend devmode identification

--- Services/Init/classes/class.ilErrorHandling.php
+++ Services/Init/classes/class.ilErrorHandling.php
@@ -262,7 +262,7 @@ class ilErrorHandling
 
     protected function isDevmodeActive(): bool
     {
-        return defined('DEVMODE') && (int) DEVMODE === 1;
+        return defined('DEVMODE') && (int) DEVMODE >= 1;
     }
 
     protected function defaultHandler(): HandlerInterface
