ab1d3e2b5abf4a890cdf26455c986eae78cdc3b3
Christina Fuchs <chr.fuchs@fau.de>
2025-03-10 11:30:18 +0100

    fix Undefined array key REMOTE_ADDR for cronjob call

--- Services/Authentication/classes/Frontend/class.ilAuthFrontend.php
+++ Services/Authentication/classes/Frontend/class.ilAuthFrontend.php
@@ -412,9 +412,9 @@ class ilAuthFrontend
                 'auth_action' => array('text', $a_action),
                 'auth_mode' => array('text', $this->getCredentials()->getAuthMode()),
                 'username' => array('text', $a_username),
-                'remote_addr' => array('text', $_SERVER['REMOTE_ADDR']),
-                'server_addr' => array('text', $_SERVER['SERVER_ADDR']),
-                'user_agent' => array('text', $_SERVER['HTTP_USER_AGENT'])
+                'remote_addr' => array('text', $_SERVER['REMOTE_ADDR'] ?? NULL),
+                'server_addr' => array('text', $_SERVER['SERVER_ADDR'] ?? NULL),
+                'user_agent' => array('text', $_SERVER['HTTP_USER_AGENT'] ?? NULL)
             )
         );
     }
