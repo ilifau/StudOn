fb388e3fa6aa219de11700fad5a20a6d517b65e9
Christina Fuchs <chr.fuchs@fau.de>
2025-04-07 18:18:18 +0200

    fau: exAssTest - remove exAssTest in class ilTestPassFinishTask as it is not working

--- Modules/Test/classes/class.ilTestPassFinishTasks.php
+++ Modules/Test/classes/class.ilTestPassFinishTasks.php
@@ -58,7 +58,7 @@ class ilTestPassFinishTasks
         $this->updateLearningProgressAfterPassFinishedIsWritten();
 
         // fau: exAssTest - call update of exercise submissions
-        $this->updateExerciseSubmissionsAfterPassFinishedIsWritten();
+        // $this->updateExerciseSubmissionsAfterPassFinishedIsWritten();
         // fau.        
     }
 
@@ -85,12 +85,12 @@ class ilTestPassFinishTasks
     /**
      * Update exercise submissions assigned to this test
      */
-    public function updateExerciseSubmissionsAfterPassFinishedIsWritten(): void
+   /* public function updateExerciseSubmissionsAfterPassFinishedIsWritten(): void
     {
         require_once ('./Modules/Exercise/AssignmentTypes/classes/class.ilExAssTypeTestResultAssignment.php');
         $test = new ilObjTest($this->obj_id, false);
         ilExAssTypeTestResultAssignment::updateAssignments($test, $this->test_session);
-    }
+    }*/
     // fau.    
 
     protected function getCaller()
