e00a939f29e7ab09307c0f821d83a500d5221223
mjansen <mjansen@databay.de>
2025-05-20 19:41:49 +0200

    App: Set release date

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -10,7 +10,7 @@
 * @package ilias-core
 */
 // lts-patch: begin version
-define("ILIAS_VERSION", "7.30.4 2025-05-20");
+define("ILIAS_VERSION", "7.30.5 2025-05-20");
 define("ILIAS_VERSION_NUMERIC", "7.30.5");
 //define("ILIAS_VERSION", "7.30 2024-05-14");
 //define("ILIAS_VERSION_NUMERIC", "7.30");			// since version ILIAS 6 this must be always x.y: x and y are numbers
