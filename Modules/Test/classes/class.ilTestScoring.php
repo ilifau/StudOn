f7d233bb8b9059b4cfef5a6a2f4691fd7fa0dddd
chrfau <chr.fuchs@fau.de>
2025-10-08 14:51:39 +0200

    fix Call to undefined method assQuestion::_updateTestResultCache()

--- Modules/Test/classes/class.ilTestScoring.php
+++ Modules/Test/classes/class.ilTestScoring.php
@@ -104,7 +104,7 @@ class ilTestScoring
             }
             if (is_object($userdata) && is_array($userdata->getPasses())) {
                 $this->recalculatePasses($userdata, $active_id);
-                assQuestion::_updateTestResultCache($active_id);
+                $this->test->updateTestResultCache($active_id);
 
                 /** @var  ilTestEvaluationUserData $userdata */
                 include_once("./Services/Tracking/classes/class.ilLPStatusWrapper.php");
