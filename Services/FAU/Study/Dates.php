31b6cd62d3214670cd22e6ce442cfcae0ae73221
chrfau <chr.fuchs@fau.de>
2026-04-13 11:21:08 +0200

    fauService - add new parameter with_instructors to function getPlannedDatesList

--- Services/FAU/Study/Dates.php
+++ Services/FAU/Study/Dates.php
@@ -47,7 +47,7 @@ class Dates
      * Get the list of planned dates for a course
      * return string[]
      */
-    public function getPlannedDatesList(?int $course_id, bool $long) : array
+    public function getPlannedDatesList(?int $course_id, bool $long, bool $with_instructors = true) : array
     {
         $list = [];
         foreach ($this->repo->getPlannedDatesOfCourse((int) $course_id) as $date) {
@@ -72,7 +72,7 @@ class Dates
             if (($long || !$this->isRegular($date)) && !empty($date->getStartdate())) {
                 $parts[] = $this->getDatespan($date->getStartdate(), $date->getEnddate());
             }
-            if ($long && !empty($instructors = $this->service->persons()->getInstructorsList($date->getPlannedDatesId()))) {
+            if ($long && $with_instructors && !empty($instructors = $this->service->persons()->getInstructorsList($date->getPlannedDatesId()))) {
                 $parts[] = implode(', ', $instructors);
             }
 
