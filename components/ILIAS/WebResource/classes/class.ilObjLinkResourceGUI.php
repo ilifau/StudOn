adb779cd8c215b2143ae6aca9dcedc391f0f92f8
chrfau <chr.fuchs@fau.de>
2026-09-23 11:47:28 +0200

    update to ilias 11 - continued

--- components/ILIAS/WebResource/classes/class.ilObjLinkResourceGUI.php
+++ components/ILIAS/WebResource/classes/class.ilObjLinkResourceGUI.php
@@ -165,6 +165,15 @@ class ilObjLinkResourceGUI extends ilObject2GUI
                 if (!$cmd) {
                     $cmd = 'view';
                 }
+
+                // fau: fixWebLinkSecurity - add permission check
+                if ($cmd != "infoScreen") {
+                    $this->checkPermission("read");
+                } else {
+                    $this->checkPermission("visible");
+                }
+                // fau.
+
                 $this->prepareOutput();
                 $this->$cmd();
         }
